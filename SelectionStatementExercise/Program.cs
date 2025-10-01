328834f7a7345660e2e770beba3763bc017bc8f5
Alexis Boegner <alexisboegner@gmail.com>
2025-09-30 22:55:11 -0400

    Stunting the Fractal

--- SelectionStatementExercise/Program.cs
+++ SelectionStatementExercise/Program.cs
@@ -1,23 +1,29 @@
-﻿namespace SelectionStatementExercise
+﻿using System.Globalization;
+
+namespace SelectionStatementExercise
 {
     public class Program
     {
         static void Main(string[] args)
         {
             int favNumber = new Random().Next(1, 100);
+            Console.WriteLine("Guess my favorite number! Hint, for this game it's an integer between 1 and 100.");
             int guess = int.Parse(Console.ReadLine());
-
-            while (guess != favNumber)
+            //Limit my loop to 15 guesses so that the user is not allowed to play the "infinite loop game!"
+            while (index[] <= index[14])
             {
                 NumberGame(favNumber);
+                index[] = index[] + 1
+            }
+
+            while (index[] == index[15])
+            {
+                NumbersSalutations(favNumber);
             }
         }
 
-        static int NumberGame(int favNumber)
+        static int NumberGame(int favNumber, int guess)
         {
-            Console.WriteLine("Guess my favorite number! Hint, for this game it's an integer between 1 and 100.");
-            int guess = int.Parse(Console.ReadLine());
-
             if (guess < favNumber)
             {
                 return Console.WriteLine($"{guess} is less than my favorite number!");
@@ -29,7 +35,16 @@
             else if (guess == favNumber)
             {
                 return Console.WriteLine("Confetti, Glitter, & Bells");
+                yield break;
             }
+        }
+
+        static int NumbersSalutations(int favNumber, int guess)
+        {
+            if (guess == favNumber)
+                return Console.WriteLine("Confetti, Glitter, & Bells");
+            else if (guess !== favNumber)
+                return Console.WriteLine($"Sorry, your out of guesses. My favorite number is {guess}");
         }
     }
 }
