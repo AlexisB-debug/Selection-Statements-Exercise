e97037eb1c11b5f61138a46982e09c3eddc17946
Alexis Boegner <alexisboegner@gmail.com>
2025-09-30 21:50:58 -0400

    Created Method NumberGame

--- SelectionStatementExercise/Program.cs
+++ SelectionStatementExercise/Program.cs
@@ -4,7 +4,32 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int favNumber = new Random().Next(1, 100);
+            int guess = int.Parse(Console.ReadLine());
+
+            while (guess != favNumber)
+            {
+                NumberGame(favNumber);
+            }
+        }
+
+        static int NumberGame(int favNumber)
+        {
+            Console.WriteLine("Guess my favorite number! Hint, for this game it's an integer between 1 and 100.");
+            int guess = int.Parse(Console.ReadLine());
+
+            if (guess < favNumber)
+            {
+                return Console.WriteLine($"{guess} is less than my favorite number!");
+            }
+            else if (guess > favNumber)
+            {
+                return Console.WriteLine($"{guess} is greater than my favorite number!");
+            }
+            else if (guess == favNumber)
+            {
+                return Console.WriteLine("Confetti, Glitter, & Bells");
+            }
         }
     }
 }
